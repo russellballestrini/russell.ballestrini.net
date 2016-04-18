c83a625a380eb2f28c18cbf85cd58e67f0dd37f4
russellballestrini <russell.ballestrini@gmail.com>
2016-04-18 18:44:28 -0400

    modified:   pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,7 @@ TIMEZONE = 'America/New_York'
 DEFAULT_LANG = u'en'
 
 # Atom all feed.
-FEED_ALL_ATOM = True
+FEED_ALL_ATOM = 'True'
 
 # Feed generation is usually not desired when developing
 CATEGORY_FEED_ATOM = None
